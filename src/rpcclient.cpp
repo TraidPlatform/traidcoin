e54c3f31cbdd010933c7f5966313040c0d6a4a74
Traid Platform <support@traid.tv>
2018-09-11 16:08:59 +0200

    Update rpcclient.cpp

--- src/rpcclient.cpp
+++ src/rpcclient.cpp
@@ -163,7 +163,7 @@ static const CRPCConvertParam vRPCConvertParams[] =
     { "checkkernel", 1 },
     { "setban", 2 },
     { "setban", 3 },
-    { "sendtostealthaddress", 1 },
+    //{ "sendtostealthaddress", 1 },
     { "searchrawtransactions", 1 },
     { "searchrawtransactions", 2 },
     { "searchrawtransactions", 3 },
