9c039975c354157b957c9e56c45da26fc32a0aee
Traid Platform <support@traid.tv>
2018-09-11 15:59:57 +0200

    Update activemasternode.cpp

--- src/activemasternode.cpp
+++ src/activemasternode.cpp
@@ -9,7 +9,7 @@
 #include "clientversion.h"
 
 //
-// Bootup the masternode, look for a 3000 TRAID input and register on the network
+// Bootup the masternode, look for a 60000 TRAID input and register on the network
 //
 void CActiveMasternode::ManageStatus()
 {
