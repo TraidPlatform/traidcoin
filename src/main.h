d7b2b7f3336e6f7444550361e42540d97e211331
Traid Platform <support@traid.tv>
2018-09-11 16:03:57 +0200

    Update main.h

--- src/main.h
+++ src/main.h
@@ -24,7 +24,7 @@ class CValidationState;
 static const int64_t DARKSEND_COLLATERAL = (0.01*COIN);
 static const int64_t DARKSEND_POOL_MAX = (4999.99*COIN);
 
-static const int64_t STATIC_POS_REWARD = 1 * COIN; //Constant reward of 3.4 TRAID per block has been ignored
+static const int64_t STATIC_POS_REWARD = 1 * COIN; //Constant reward of 1 TRAID per block has been ignored
 static const int64_t TARGET_SPACING = 2 * 60; // 2 min per Block
 //static const int64_t TARGET_SPACING2 = 53; // 53 sec per Block
 //static const int64_t STAKE_TIMESPAN_SWITCH_TIME = 1508858115;
